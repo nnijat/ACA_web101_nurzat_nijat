b6697a48aeaaec215f3be963d6d57f30c7439877
nnijat <nn.aca2019@gmail.com>
2019-03-16 23:58:25 -0500

    js practices

---
+++ MYPORTFOLIO/JSPractice/PrintArrayWords.js
@@ -0,0 +1,17 @@
+const wordArr = [
+  "waffle",
+  "whimper",
+  "sneeze",
+  "shindig",
+  "leopard",
+  "whisper",
+  "elegant"
+];
+
+const printOutWord = arr => {
+  for (i = 0; i < wordArr.length; i++) {
+    console.log(wordArr[i]);
+  }
+};
+
+//printOutWord(wordArr)
